494887a8a590251180d37b19c7beabdf9b8a1fbd
Kevin <emkf_95@hotmail.com>
2025-02-09 00:03:48 +0100

    2024/02/09 - Settings Update

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,15 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: [
-    'expo',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-native/all',
-    'prettier'
+    'expo', 'prettier'
   ],
   ignorePatterns: ['/dist/*'],
   plugins: ["eslint", "prettier"],
   rules: {
-    'prettier': ['error'],
+    'prettier/prettier': ['error'],
   }
 };
